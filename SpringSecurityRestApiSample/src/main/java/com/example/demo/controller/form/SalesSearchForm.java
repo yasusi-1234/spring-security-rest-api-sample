87730bb1848b62a89bbdc765e1c8ef5c9ed9b3f4
yasu <yasusi0117@live.jp>
2021-07-28 11:08:08 +0900

    リクエストで受け取るFormクラス追加(Salesテーブル用)

---
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/controller/form/SalesSearchForm.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller.form;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 売上データ抽出用のフォーム
+ *
+ */
+@Data
+@ToString
+public class SalesSearchForm {
+	/** カテゴリー名 */
+	private String categoryName;
+	/** 製品名 */
+	private String productName;
+	/** 期間抽出用、抽出したい開始時間 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime fromDate;
+	/** 期間抽出用、抽出したい終了時間 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime toDate;
+	/** 期間抽出用、決まった日付の抽出用 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime fixedDate;
+
+}
