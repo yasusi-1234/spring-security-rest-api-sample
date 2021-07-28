5f4744c4be3eb0fef400abbdb939017dacd2b36a
yasu <yasusi0117@live.jp>
2021-07-28 11:25:38 +0900

    フィールドの追加

--- SpringSecurityRestApiSample/src/main/java/com/example/demo/common/EntityValues.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/common/EntityValues.java
@@ -4,6 +4,9 @@ public class EntityValues {
 
 	public static final String ROLE = "role";
 	public static final String PRODUCT = "product";
+	public static final String PRODUCT_NAME = "productName";
 	public static final String EMPLOYEE = "employee";
 	public static final String CATEGORY = "category";
+	public static final String CATEGORY_NAME = "categoryName";
+	public static final String SALES_TIME = "salesTime";
 }
