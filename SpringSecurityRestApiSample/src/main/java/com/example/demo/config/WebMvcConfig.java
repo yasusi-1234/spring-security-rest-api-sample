c98cce83fb25cd519fe14b3bb9bf305fdd7a7bf7
yasu <yasusi0117@live.jp>
2021-07-28 10:45:44 +0900

    ModelマッパーBeanを厳格化モードに変更

--- SpringSecurityRestApiSample/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,10 @@ public class WebMvcConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 厳格モード
+//		mapper.getConfiguration().setFullTypeMatchingRequired(false);
+		return mapper;
 	}
+
 }
