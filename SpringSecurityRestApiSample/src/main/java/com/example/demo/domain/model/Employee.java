ba84310f99871f2cc6b84bffe078e13c269c41ff
yasu <yasusi0117@live.jp>
2021-08-07 16:02:16 +0900

    カラム情報の追加

--- SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/model/Employee.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/model/Employee.java
@@ -1,13 +1,13 @@
 package com.example.demo.domain.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,19 +19,26 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Employee {
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = -5952602149226260392L;
+
 	@Id
 	private String employeeId;
-	@Column(unique = true)
+	@Column(unique = true, nullable = false)
 	private String mailAddress;
 	@JsonIgnore
+	@Column(nullable = false)
 	private String password;
+	@Column(nullable = false)
 	private String firstName;
+	@Column(nullable = false)
 	private String lastName;
+	@Column(nullable = false)
 	private int age;
+	@Column(nullable = false)
 	private LocalDate joiningDate;
-	@OneToOne(fetch = FetchType.EAGER, optional = false)
+	@ManyToOne(optional = false)
 	@JoinColumn(name = "role_id")
-//	@Fetch(FetchMode.JOIN)
 	private Role role;
 }
