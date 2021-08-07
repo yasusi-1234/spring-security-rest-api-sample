761364114975cf2507a99693f18ed0f802de4f3d
yasu <yasusi0117@live.jp>
2021-08-07 16:09:23 +0900

    処理、コンストラクタの調整

--- SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/service/exception/ProductStockUpdateFailureException.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/service/exception/ProductStockUpdateFailureException.java
@@ -1,5 +1,12 @@
 package com.example.demo.domain.service.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.demo.domain.model.Product;
+
+import lombok.Getter;
+
 /**
  * 製品の更新処理に失敗した場合に呼び出される例外
  *
@@ -8,7 +15,17 @@ public class ProductStockUpdateFailureException extends RuntimeException {
 
 	private static final long serialVersionUID = 8842750012957018261L;
 
+	@Getter
+	Map<String, Object> map = new HashMap<>();
+
 	public ProductStockUpdateFailureException(String message) {
 		super(message);
+		map.put("message", message);
+	}
+
+	public ProductStockUpdateFailureException(String message, Product product) {
+		super(message);
+		map.put("message", message);
+		map.put("product", product);
 	}
 }
