f72b38ad7fd963b1dc0e056f48791729ac351cd3
yasu <yasusi0117@live.jp>
2021-08-07 16:10:32 +0900

    製品更新用・作成用のメソッドの追加

--- SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/service/ProductService.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/service/ProductService.java
@@ -2,8 +2,10 @@ package com.example.demo.domain.service;
 
 import java.util.List;
 
-import com.example.demo.controller.form.ProductForm;
+import com.example.demo.controller.form.product.ProductForm;
+import com.example.demo.controller.form.product.UpdateProductsForm;
 import com.example.demo.domain.model.Product;
+import com.example.demo.domain.service.exception.ProductStockUpdateFailureException;
 
 public interface ProductService {
 
@@ -13,6 +15,16 @@ public interface ProductService {
 
 	Product save(ProductForm productForm);
 
-	int updateStock(String productId, int subtractedValue);
+	Product updateSubscribeStock(String productId, int subtractedValue);
+
+	Product updateAddStock(String productId, int subtractedValue);
+
+	/**
+	 * 製品情報更新用メソッド(供給用) 製品IDと在庫増加数の情報を元に製品の数量を複数更新する
+	 * 
+	 * @throws ProductStockUpdateFailureException リクエストされた商品IDがDBに存在しない場合に発生する
+	 * 
+	 */
+	List<Product> multiplAddStock(UpdateProductsForm form);
 
 }
--- SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/service/ProductServiceImpl.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/service/ProductServiceImpl.java
@@ -3,18 +3,22 @@ package com.example.demo.domain.service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.controller.form.ProductForm;
+import com.example.demo.controller.form.product.ProductForm;
+import com.example.demo.controller.form.product.UpdateProductsForm;
 import com.example.demo.domain.model.Category;
 import com.example.demo.domain.model.Product;
 import com.example.demo.domain.repository.ProductRepository;
 import com.example.demo.domain.service.exception.ProductStockUpdateFailureException;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
 public class ProductServiceImpl implements ProductService {
@@ -22,13 +26,6 @@ public class ProductServiceImpl implements ProductService {
 	private final ProductRepository productRepository;
 
 	private final ModelMapper modelMapper;
-
-	@Autowired
-	public ProductServiceImpl(ProductRepository productRepository, ModelMapper modelMapper) {
-		super();
-		this.productRepository = productRepository;
-		this.modelMapper = modelMapper;
-	}
 
 	/**
 	 * DBに登録されている全件分の製品情報を検索し、返却するメソッド
@@ -68,17 +65,58 @@ public class ProductServiceImpl implements ProductService {
 	 * 製品情報更新用メソッド(削減用) 製品IDと在庫削減数の情報を元に製品の数量を更新する
 	 * 
 	 * @throws ProductStockUpdateFailureException 現在のDBの在庫数より多い数値が指定された場合に発生する
+	 *                                            またリクエストされた商品IDがDBに存在しない場合も発生する
 	 */
 	@Transactional(readOnly = false, rollbackFor = Exception.class)
 	@Override
-	public int updateStock(String productId, int subtractedValue) {
+	public Product updateSubscribeStock(String productId, int subtractedValue) {
 		Product nowProduct = findById(productId);
+		if (nowProduct == null) {
+			throw new ProductStockUpdateFailureException(
+					"request update failed." + "request productId not found. requestId: " + productId);
+		}
 		if (nowProduct.getStock() < subtractedValue) {
 			throw new ProductStockUpdateFailureException(
 					"request update failed. now repository stock is " + nowProduct.getStock() + ". but request"
-							+ " subtractedValue is " + subtractedValue + ". Need to rewrite the value to be reduced.");
+							+ " subtractedValue is " + subtractedValue + ". Need to rewrite the value to be reduced.",
+					nowProduct);
 		}
-		return productRepository.updateProductStock(productId, subtractedValue);
+		// 失敗するとMysqlDataTruncation
+		productRepository.updateSubscribeProductStock(productId, subtractedValue);
+		return findById(productId);
+	}
+
+	/**
+	 * 製品情報更新用メソッド(供給用) 製品IDと在庫増加数の情報を元に製品の数量を更新する
+	 * 
+	 * @throws ProductStockUpdateFailureException リクエストされた商品IDがDBに存在しない場合に発生する
+	 * 
+	 */
+	@Transactional(readOnly = false, rollbackFor = Exception.class)
+	@Override
+	public Product updateAddStock(String productId, int addValue) {
+		Product nowProduct = findById(productId);
+		if (nowProduct == null) {
+			throw new ProductStockUpdateFailureException(
+					"request update failed." + "request productId not found. requestId: " + productId);
+		}
+		productRepository.updateAddProductStock(productId, addValue);
+		return findById(productId);
+	}
+
+	/**
+	 * 製品情報更新用メソッド(供給用) 製品IDと在庫増加数の情報を元に製品の数量を複数更新する
+	 * 
+	 * @throws ProductStockUpdateFailureException リクエストされた商品IDがDBに存在しない場合に発生する
+	 * 
+	 */
+	@Transactional(readOnly = false, rollbackFor = Exception.class)
+	@Override
+	public List<Product> multiplAddStock(UpdateProductsForm form) {
+		List<Product> responseProduct = form.getUpdateProducts().stream()
+				.map(requestData -> updateAddStock(requestData.getProductId(), requestData.getRequestValue()))
+				.collect(Collectors.toList());
+		return responseProduct;
 	}
 
 }
