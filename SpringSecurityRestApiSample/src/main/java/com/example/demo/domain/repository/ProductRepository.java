4f1c3dd8d2660aa47ab52f530a1c9724b311c906
yasu <yasusi0117@live.jp>
2021-08-07 16:11:44 +0900

    検索用Query,UpdateようQueryの追加

--- SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/repository/ProductRepository.java
+++ SpringSecurityRestApiSample/src/main/java/com/example/demo/domain/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +11,17 @@ import com.example.demo.domain.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
 
-	@Modifying
+	/* ロックを掛けるかの検討をする */
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
 	@Query(nativeQuery = true, value = "UPDATE product SET stock = stock - :subtractedValue WHERE product_id = :productId")
-	public int updateProductStock(@Param("productId") String productId, @Param("subtractedValue") int subtractedValue);
+	public int updateSubscribeProductStock(@Param("productId") String productId,
+			@Param("subtractedValue") int subtractedValue);
+
+	/* ロックを掛けるかの検討をする */
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Query(nativeQuery = true, value = "UPDATE product SET stock = stock + :addValue WHERE product_id = :productId")
+	public int updateAddProductStock(@Param("productId") String productId, @Param("addValue") int addValue);
+
+	@Query("SELECT pro, ca FROM Product pro INNER JOIN FETCH Category ca ON pro.category = ca.categoryId WHERE pro.productId IN :productIds")
+	public List<Product> findByProductIds(@Param("productIds") List<String> productIds);
 }
